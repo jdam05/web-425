81888173ae5116dc4742b447fdaa84b6ba3d43aa
jdam05 <j.damir05@gmail.com>
2023-01-15 22:26:31 -0500

    Weeek-2 Submission

---
+++ week-2/composer-app/src/app/composer-list/composer-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+export default class Composer {
+  fullName: string;
+  genre: string;
+
+  constructor(fullName: string, genre: string) {
+    this.fullName = fullName;
+    this.genre = genre;
+  }
+}
+
+@Component({
+  selector: 'app-composer-list',
+  templateUrl: './composer-list.component.html',
+  styleUrls: ['./composer-list.component.css'],
+})
+export class ComposerListComponent implements OnInit {
+  composers: Array<Composer>;
+
+  constructor() {
+    this.composers = [new Composer('Luodvico Einaudi', 'Classical')];
+  }
+
+  ngOnInit(): void {}
+}
