ba3c64a31d24db8e266c3fa612e4f35c5c849083
jdam05 <j.damir05@gmail.com>
2023-01-15 19:47:03 -0500

    Updated for Week-2 Exercise 2.3

--- week-1/profile-app/src/app/my-details/my-details.component.ts
+++ week-1/profile-app/src/app/my-details/my-details.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+export default class Person {
+  fullName: string;
+  favoriteFood: string;
+  favoriteColor: string;
+  keywords = ['#TypeScript', '#2023', 'CodingWithAngular', '#ngOmaha'];
+  constructor(fullName: string, favoriteFood: string, favoriteColor: string) {
+    this.fullName = fullName;
+    this.favoriteFood = favoriteFood;
+    this.favoriteColor = favoriteColor;
+  }
+
+  toString() {
+    console.log(
+      `\n Full name: ${this.fullName}\n Favorite food: ${this.favoriteFood}\n Favorite color: ${this.favoriteColor}`
+    );
+  }
+}
 @Component({
   selector: 'app-my-details',
   templateUrl: './my-details.component.html',
-  styleUrls: ['./my-details.component.css']
+  styleUrls: ['./my-details.component.css'],
 })
-export class MyDetailsComponent {
+export class MyDetailsComponent implements OnInit {
+  myProfile: Person;
 
+  constructor() {
+    this.myProfile = new Person('Jamal Damir', 'Couscous', 'green');
+    this.myProfile.toString();
+  }
+  ngOnInit(): void {}
 }
