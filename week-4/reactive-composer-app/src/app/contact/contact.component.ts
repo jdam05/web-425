405d0511d7a488938a7029315eadc0aceb4acfac
jdam05 <j.damir05@gmail.com>
2023-01-25 22:07:11 -0500

    Week-4 Exercise 4.3 Submission

---
+++ week-4/reactive-composer-app/src/app/contact/contact.component.ts
@@ -0,0 +1,19 @@
+/**
+ * Title: contact.component.ts
+ * Date: January 12, 2023
+ * Author: Jamal Eddine Damir
+ * Description: Composer contact component
+ * Sources:
+ * Source code from class GitHub Repository
+ * Instructor provided assignment specific instructions
+ */
+
+// Importing component
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css'],
+})
+export class ContactComponent {}
