92d06896e423598b9c1087890f95d64942c351f6
jdam05 <j.damir05@gmail.com>
2023-02-09 00:01:29 -0500

    Assignment 6.3 Submission

--- week-6/gpa-calculator-app1/src/app/base-layout/base-layout.component.ts
+++ week-6/gpa-calculator-app1/src/app/base-layout/base-layout.component.ts
@@ -1,5 +1,17 @@
+/**
+ * Title: base-layout.component.ts
+ * Date: February 8, 2023
+ * Author: Jamal Eddine Damir
+ * Description: Base layout component for gpa-calculator-app
+ * Sources:
+ * Source code from class GitHub Repository
+ * Instructor provided assignment specific instructions
+ */
+
+// Import Component object
 import { Component } from '@angular/core';
 
+// defining component's metadata
 @Component({
   selector: 'app-base-layout',
   templateUrl: './base-layout.component.html',
